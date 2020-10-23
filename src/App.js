a5e427c9c24d05dc229da2602cfb292c8ff0f81f
magneka <magne@alvheim.com>
2020-10-23 11:17:59 +0200

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./style.css";
+import useAxiosGet from "./useAxiosGet"
 
 export default function App() {
+
+  [getData, state] = useAxiosGet()
+
+  const getFakturaliste = () => {
+    getData("http://localhost:5000/api/sak/FakturalisteForSak/1231234")
+  }
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+
+       <button onClick={getFakturaliste}>fakturaListe</button>
     </div>
   );
 }
---
+++ useAxiosGet.js
@@ -0,0 +1,66 @@
+import React, { useReducer } from 'react';
+import axios from "axios";
+
+
+const useAxiosGet = (() => {
+
+  const actions = {
+    LOADING: 'LOADING',
+    DATA: 'DATA',
+    ERROR: 'ERROR',
+  } 
+
+  const dataReducer = (state, action) => {
+    
+    console.log(JSON.stringify(action))
+    
+    switch (action.type) {
+      case actions.LOADING:
+        return {
+
+          ...state,
+          loading: true,
+          error: null
+        };
+      case actions.DATA:
+        return {
+          loading: false,
+          data: {result: action.data},
+          error: action.error
+        };
+      case actions.ERROR:
+        return {
+          ...state,
+          loading: false,          
+          error: action.error
+        };
+      
+      default:
+        return state;
+    }
+  }
+
+  const initialState = {
+    data: {},
+    loading: false,
+    error: false
+  }  
+
+  const [state, dispatch] = useReducer(dataReducer, initialState)
+
+  const getData = (props) => {      
+    axios.get(props.uri)
+    .then(result => {
+      console.log(result);
+      dispatch({ type: actions.DATA, data: result.data });
+    })
+    .catch(error => {
+      console.error("error: ", error);
+      dispatch({ type: actions.ERROR, error: error });        
+    });
+  } 
+
+  return [getData, state]
+})
+
+export default useAxiosGet
